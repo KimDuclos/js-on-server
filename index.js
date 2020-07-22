8a57a386154e82cf60aa16b61a5f76d56a1d914a
KimDuclos <kimduclos@gmail.com>
2020-07-22 13:43:45 -0600

    working

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require('express'); // require express in app
-const app = express(); // app is express as a function
-const fetch = require("node-fetch");
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
 
 function getCryptos(code) {
-    return fetch('https://api.nexchange.io/en/api/v1/currency/')
-        .then(cryptoData => cryptoData.json())
-        .then(cryptoData => {
-            return cryptoData.filter(crypto => crypto.code == code); // remember filter syntax => if crypto.code = code, return that data
-        })        
-        .catch(err => console.log(err));
+	return fetch('https://api.nexchange.io/en/api/v1/currency/')
+		.then(cryptoData => cryptoData.json())
+		.then(cryptoData => {
+			return code ? cryptoData.filter(crypto => crypto.code == code) : cryptoData;
+		})
+		.catch(err => console.log(err));
 }
 app.get('/', (req, res) => {
-    const code = req.query.code; // set code to requested code from API 
-    getCryptos(code)  // call getCryptos function
-        .then(cryptoData => {  // render home.ejs with filtered data from function
-            res.render('home', { cryptoData: cryptoData });
-        })
-        .catch(err => console.log(err))
+	const code = req.query.code;
+	console.log(code);
+	getCryptos(code)
+		.then(cryptoData => {
+			res.render('home', { cryptoData: cryptoData });
+		})
+		.catch(err => console.log(err));
 });
+
+app.set('view engine', 'ejs');
 
 app.listen(3000, () => {
-	// app is listening on port 3000 and using function handleIndexRequest
 	console.log('Example app listening on port 3000!');
 });
-
-app.set('view engine', 'ejs'); // app calls ejs dependency
